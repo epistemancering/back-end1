a8a262cdf15d68ac79a5b391e8516b13b855673d
Austin Henrie <nitsua15@gmail.com>
2023-04-29 21:47:09 -0600

    minor correction

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.listen(4000, () => console.log("Server running on port 4000"));
 app.get("/api/users", (req, res) => {
     let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
     res.status(200).send(friends);
@@ -12,3 +11,4 @@ app.get("/weather/:temperature", (req, res) => {
     const phrase = `<h3>It was ${req.params.temperature} today</h3>`;
     res.status(200).send(phrase);
 });
+app.listen(4000, () => console.log("Server running on port 4000"));
